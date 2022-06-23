f3076cf73041245bee4a756aad05e5d23437d07c
Nursultan <nursuccessmj@gmail.com>
2022-06-23 09:37:12 +0600

    edited modal

--- src/js/main.js
+++ src/js/main.js
@@ -17,18 +17,3 @@ const burger = document.querySelector('.navbar-toggler');
 burger.addEventListener('click', () => {
   document.querySelector('#navbarNav').classList.toggle('collapse')
 })
-
-// const login = document.querySelector('#login');
-
-// login.addEventListener('click', (e) => {
-//   e.preventDefault();
-
-  
-// })
-
-let myModal = document.getElementById('myModal')
-let myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
