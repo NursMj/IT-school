2ae30104d4e130528c73fe61e7fcc83b6880d956
Nursultan <nursuccessmj@gmail.com>
2022-06-27 14:17:57 +0600

    set modal

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,17 @@ const burger = document.querySelector('.navbar-toggler');
 burger.addEventListener('click', () => {
   document.querySelector('#navbarNav').classList.toggle('collapse')
 })
+
+const modal = document.querySelector('#modal-form'),
+      modalEmail = document.querySelector('.modal-email'),
+      modalPsw = document.querySelector('.modal-psw');
+
+modal.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (modalEmail.value === 'beka@gmail.com' && modalPsw.value === 'beka123456') {
+    window.location = 'personal.html'
+  } else {
+    e.target.reset();
+    document.querySelector('.error-message').classList.add('error-message_active');
+  }
+})
